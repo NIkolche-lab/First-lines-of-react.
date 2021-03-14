e439063c30a828a08a62a0fb71df466a549851bb
Nikolche <63500959+NIkolche-lab@users.noreply.github.com>
2021-03-14 20:08:06 +0100

    react

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import Clock from './Components/Clock';
+import Blogpost from './Components/Blogpost';
 
 
 function App() {
   return (
     <div>
       <Clock/>
-      
+      <Blogpost/>
     </div>
     
   );
